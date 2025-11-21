2cf2f781b56298a9bae4f62dc6b6c7398306c446
RedShadow <85443694+SIM0N0URI@users.noreply.github.com>
2025-11-21 15:19:47 +0100

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 6.1 MB (18.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,13 @@
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums)-1; i++ {
-        for j := i+1; j < len(nums); j++ {
-            if nums[i] + nums[j] == target {
-                return []int{i, j}
-            }
+    newmap:=make(map[int]int)
+    for i,num:=range nums{
+        take:=target-num
+        if j,get:=newmap[take];get{
+            return []int{j,i}
         }
+        newmap[num]=i
     }
-    return []int{}
+
+	return nil 
 }
+
